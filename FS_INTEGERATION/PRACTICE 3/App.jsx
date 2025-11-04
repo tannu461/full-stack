66c2ff61ca70935d1934f8b52947f0e141a7e602
Tannu <tannuku706@gmail.com>
2025-11-04 12:30:53 +0530

    Create App component to render ChatRoom

---
+++ FS_INTEGERATION/PRACTICE 3/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ChatRoom from './ChatRoom';
+
+export default function App() {
+  return (
+    <div style={{display:'flex', justifyContent:'center', padding:20}}>
+      <ChatRoom />
+    </div>
+  );
+}
